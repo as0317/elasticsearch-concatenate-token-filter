e91061562ec1ddac6b36b4925fd1ca69a9d0c77f
as0317 <as0317@yp.com>
2015-12-16 11:43:43 -0800

    Update ConcatenatePlugin.java

--- src/main/java/elasticsearch/concatenate/ConcatenatePlugin.java
+++ src/main/java/elasticsearch/concatenate/ConcatenatePlugin.java
@@ -1,6 +1,7 @@
 package elasticsearch.concatenate;
 
 import java.util.Collection;
+import org.elasticsearch.index.analysis.AnalysisModule;
 
 import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
@@ -15,5 +16,8 @@ public class ConcatenatePlugin extends Plugin {
     @Override
     public String description() {
         return "Plugin that provides a Token Filter that recombines all of the tokens in a token stream back into one.";
+    }
+    public void onModule(AnalysisModule module) {
+        module.addTokenFilter("concatenate", ConcatenateTokenFilterFactory.class);
     }
 }
